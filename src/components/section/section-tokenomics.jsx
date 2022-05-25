7029eba827690053ba12b6a5522cf937e5712867
Ahsan Iqbal <axan.iqbal1234@gmail.com>
2022-05-25 18:00:47 +0500

    typo

--- src/components/section/section-tokenomics.jsx
+++ src/components/section/section-tokenomics.jsx
@@ -57,7 +57,7 @@ function SectionTokenomics() {
             <Grid container>
                 <Grid item xs={12} sm={6}>
                     <Stack >
-                        <Typography variant={"body1"} paddingY={1}>$666 is an BEP20 token which started on the
+                        <Typography variant={"body1"} paddingY={1}>$666 is a BEP20 token which started on the
                             BINANCE SMART CHAIN blockchain MAY 26, 2021.
                             The max supply is 666 trillion and one of its key
                             functionalities is its decentralized finance protocol.</Typography>
