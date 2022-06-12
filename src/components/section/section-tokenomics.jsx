5b559c9d782f429d75c2f7834978a4e71711753d
Ahsan Iqbal <axan.iqbal1234@gmail.com>
2022-06-12 21:33:20 +0500

    changes

--- src/components/section/section-tokenomics.jsx
+++ src/components/section/section-tokenomics.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Section from "../section";
-import {Grid, Stack, styled, Typography as MuiTypography} from "@mui/material";
-import {PieChart} from "react-minimal-pie-chart";
+import {Box, Grid, Stack, styled, Typography as MuiTypography} from "@mui/material";
+import {Pie, ResponsiveContainer, PieChart, Tooltip, Legend} from "recharts";
 
 const MySection = styled(Section)`
   background: linear-gradient(141deg, rgba(47, 79, 79, 0.8) 10%, rgba(54, 21, 14, 0.8) 50%);
@@ -15,6 +15,19 @@ const Typography = styled(MuiTypography)`
   text-shadow: rgb(0, 0, 0) 3px 5px 2px;
 `
 
+const CustomTooltip = ({ active, payload }) => {
+    console.log(active,payload)
+    if (active && payload && payload.length) {
+        return (
+            <Box>
+                <Typography>{`${payload[0].name}  ${payload[0].value}%`}</Typography>
+            </Box>
+        );
+    }
+
+    return null;
+};
+
 function SectionTokenomics() {
     const colors = [
         "rgba(47, 79, 79, 0.7)",
@@ -26,22 +39,22 @@ function SectionTokenomics() {
         {
             "title": "OPERATIONS (CEX)",
             "value": 10,
-            "color": colors[0]
+            "fill": colors[0]
         },
         {
             "title": "STAKING",
             "value": 10,
-            "color": colors[1]
+            "fill": colors[1]
         },
         {
             "title": "P2E",
             "value": 10,
-            "color": colors[2]
+            "fill": colors[2]
         },
         {
             "title": "PRESALE/LAUNCH",
             "value": 70,
-            "color": colors[3]
+            "fill": colors[3]
         },
     ];
     return (
@@ -66,47 +79,66 @@ function SectionTokenomics() {
                         </Stack>
                     </Grid>
                     <Grid item xs={12} md={6}>
-                        <PieChart
-                            data={data01}
-                            startAngle={280}
-                            // label={({ dataEntry }) => `${dataEntry.title}\n${dataEntry.value}%`}
-                            label={({x, y, dx, dy, dataEntry, textAnchor, dataIndex}) => {
-                                const transform = [
-                                    "rotate(-60deg) translate(-51%, 40%)",
-                                    "rotate(-27deg) translate(-22%, 29%)",
-                                    "rotate(4deg) translate(9%, -5%)",
-                                    null,
-                                ]
-                                return <text
-                                    x={x}
-                                    y={y}
-                                    dx={dx}
-                                    dy={dy}
-                                    dominantBaseline={"central"}
-                                    textAnchor={textAnchor}
+                        {/*<PieChart*/}
+                        {/*    data={data01}*/}
+                        {/*    label={({dataEntry})=> `${dataEntry.title} ${dataEntry.value}%`}*/}
+                        {/*    animate*/}
+                        {/*    labelStyle={(index) => ({*/}
+                        {/*        fill: "whitesmoke",*/}
+                        {/*        fontSize: '3.4px',*/}
+                        {/*    })}*/}
+                        {/*    lineWidth={20}*/}
+                        {/*    rounded*/}
+                        {/*    paddingAngle={15}*/}
+                        {/*    labelPosition={79}*/}
+                        {/*    startAngle={120}*/}
+                        {/*/>*/}
+                        {/*<PieChart*/}
+                        {/*    data={data01}*/}
+                        {/*    startAngle={280}*/}
+                        {/*    // label={({ dataEntry }) => `${dataEntry.title}\n${dataEntry.value}%`}*/}
+                        {/*    label={({x, y, dx, dy, dataEntry, textAnchor, dataIndex}) => {*/}
+                        {/*        const transform = [*/}
+                        {/*            "rotate(-60deg) translate(-51%, 40%)",*/}
+                        {/*            "rotate(-27deg) translate(-22%, 29%)",*/}
+                        {/*            "rotate(4deg) translate(9%, -5%)",*/}
+                        {/*            null,*/}
+                        {/*        ]*/}
+                        {/*        return <text*/}
+                        {/*            x={x}*/}
+                        {/*            y={y}*/}
+                        {/*            dx={dx}*/}
+                        {/*            dy={dy}*/}
+                        {/*            dominantBaseline={"central"}*/}
+                        {/*            textAnchor={textAnchor}*/}
+                        {/*            style={{*/}
+                        {/*                fill: "whitesmoke",*/}
+                        {/*                fontSize: '4px',*/}
+                        {/*                transform: transform[dataIndex]*/}
+                        {/*            }}*/}
+                        {/*        >*/}
+                        {/*            {dataEntry.title + ' ' + Math.round(dataEntry.percentage) + '%'}*/}
+                        {/*        </text>*/}
+                        {/*    }}*/}
+                        {/*    labelPosition={50}*/}
+                        {/*/>*/}
+                        <ResponsiveContainer height={"100%"} width={"100%"} minHeight={600} >
+                            <PieChart>
+
+                                <Pie
+                                    data={data01}
+                                    dataKey="value"
+                                    nameKey="title"
+                                    legendType="diamond"
                                     style={{
-                                        fill: "whitesmoke",
-                                        fontSize: '4px',
-                                        transform: transform[dataIndex]
+                                        fontSize: 20,
                                     }}
-                                >
-                                    {dataEntry.title + ' ' + Math.round(dataEntry.percentage) + '%'}
-                                </text>
-                            }}
-                            // labelStyle={(index) => ({
-                            //     fill: "whitesmoke",
-                            //     fontSize: '4px',
-                            //
-                            // })}
-                            labelPosition={50}
-                        />
-                        {/*<ResponsiveContainer height={"100%"} width={"80%"} minHeight={200} minWidth={200}>*/}
-                        {/*    <PieChart>*/}
-                        {/*        <Pie data={data01} dataKey={"value"} innerRadius={"40%"} outerRadius={"70%"} nameKey={"name"} label={props => `${props.name} ${props.value}%`}>*/}
-                        {/*            {data01.map((value, index) => <Cell key={index} fill={colors[index]}/>)}*/}
-                        {/*        </Pie>*/}
-                        {/*    </PieChart>*/}
-                        {/*</ResponsiveContainer>*/}
+                                    label={props => `${props.value}%`}
+                                />
+                                <Tooltip content={<CustomTooltip/>}/>
+                                <Legend />
+                            </PieChart>
+                        </ResponsiveContainer>
                     </Grid>
                 </Grid>
             </Stack>
